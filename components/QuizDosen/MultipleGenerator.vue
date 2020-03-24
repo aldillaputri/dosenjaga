d7642bae47fabec3397b35705c3905013a51175e
aldillaputri <aldillaputri5@gmail.com>
2020-03-24 22:23:10 +0700

    perbaikan kuis kombinasi essay dan pilgan

--- components/QuizDosen/MultipleGenerator.vue
+++ components/QuizDosen/MultipleGenerator.vue
@@ -4,22 +4,18 @@
       <v-form id="form-tambah-soal" ref="form" lazy-validation>
         <div><v-icon>mdi-forum</v-icon>&nbsp;Deskripsi Kuis</div>
         <v-select
-          v-model="editedItem.kuliah"
+          v-model="form.kuliah"
           :items="kuliah"
           item-text="matkul"
           item-value="nomor"
           label="Kuliah"
-          @change="refresh_dropdown"
-        ></v-select>
-        <v-text-field
-          v-model="editedItem.judul"
-          label="Judul Kuis"
-        ></v-text-field>
+        />
+        <v-text-field v-model="form.judul" label="Judul Kuis" />
         <!--datepicker-->
         <v-row>
           <v-col cols="12" sm="6" md="4">
             <v-menu
-              v-model="menuDate"
+              v-model="menuDatePicker"
               :close-on-content-click="false"
               :nudge-right="40"
               transition="scale-transition"
@@ -28,25 +24,22 @@
             >
               <template v-slot:activator="{ on }">
                 <v-text-field
-                  v-model="editedItem.date_created"
+                  v-model="form.date_created"
                   label="Tanggal Mulai Kuis"
                   readonly
                   v-on="on"
                 ></v-text-field>
               </template>
               <v-date-picker
-                v-model="editedItem.date_created"
-                @input="menuDate = false"
-              ></v-date-picker>
+                v-model="form.date_created"
+                @input="menuDatePicker = false"
+              />
             </v-menu>
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="12" sm="6" md="4">
-            <v-text-field
-              v-model="editedItem.durasi"
-              label="Durasi"
-            ></v-text-field>
+            <v-text-field v-model="form.durasi" label="Durasi"></v-text-field>
           </v-col>
         </v-row>
         <!-- expansion panel -->
@@ -56,10 +49,7 @@
               <v-expansion-panel>
                 <v-expansion-panel-header>
                   <template v-slot:default="{ open }">
-                    <v-row
-                      v-if="line.tipe == 'Pilihan Ganda' || 'Essay'"
-                      no-gutters
-                    >
+                    <v-row v-if="line.tipe == 'Pilihan Ganda'" no-gutters>
                       <v-col cols="4">Soal</v-col>
                       <v-col cols="8" class="text--secondary">
                         <v-fade-transition leave-absolute>
@@ -74,7 +64,6 @@
                   <v-row id="row-">
                     <v-col>
                       <v-file-input
-                        v-if="line.tipe == 'Pilihan Ganda' || 'Essay'"
                         v-model="line.gambar"
                         show-size
                         counter
@@ -86,7 +75,6 @@
                   <v-row id="row-">
                     <v-col>
                       <v-text-field
-                        v-if="line.tipe == 'Pilihan Ganda' || 'Essay'"
                         v-model="line.pertanyaan"
                         label="Pertanyaan"
                       ></v-text-field>
@@ -95,7 +83,6 @@
                   <v-row>
                     <v-col>
                       <v-text-field
-                        v-if="line.tipe == 'Pilihan Ganda' || 'Essay'"
                         v-model="line.jawaban1"
                         label="Jawaban"
                       ></v-text-field>
@@ -131,7 +118,6 @@
                   <v-row>
                     <v-col cols="3">
                       <v-text-field
-                        v-if="line.tipe == 'Pilihan Ganda' || 'Essay'"
                         v-model="line.bobot"
                         label="Bobot"
                       ></v-text-field>
@@ -142,7 +128,7 @@
                       <v-select
                         v-if="line.tipe == 'Pilihan Ganda'"
                         v-model="line.kunci"
-                        :items="items"
+                        :items="kuncis"
                         label="Kunci Jawaban"
                         dense
                         chips
@@ -164,17 +150,9 @@
         </div>
         <v-icon>mdi-chevron-double-down</v-icon>&nbsp;Soal Berikutnya
         <v-col>
-          <v-radio-group v-model="radio_grup" row>
-            <v-radio
-              label="Pilihan Ganda"
-              value="Pilihan Ganda"
-              @change="getValueRadioGrup('Pilihan Ganda')"
-            ></v-radio>
-            <v-radio
-              label="Essay"
-              value="Essay"
-              @change="getValueRadioGrup('Essay')"
-            ></v-radio>
+          <v-radio-group v-model="tipe_soal" row>
+            <v-radio label="Pilihan Ganda" value="Pilihan Ganda"></v-radio>
+            <v-radio label="Essay" value="Essay"></v-radio>
           </v-radio-group>
           <v-btn outlined color="indigo" small @click="addQuestion"
             >Tambah Soal</v-btn
@@ -185,7 +163,7 @@
           <b>Total Point:</b>
           <v-spacer></v-spacer>
           <v-btn color="error" to="/">Batal</v-btn>
-          <v-btn color="normal" @click="reset">Reset</v-btn>
+          <v-btn color="normal">Reset</v-btn>
           <v-btn color="success" @click="save">Buat</v-btn>
         </v-card-actions>
       </v-form>
@@ -197,17 +175,17 @@
         <v-icon>mdi-plus-circle</v-icon>&nbsp;Tambah Dari Bank Soal
       </div>
       <v-row dense>
-        <v-col v-for="(item, i) in items" :key="i" cols="12">
+        <v-col v-for="(soal, i) in soal_tersedia" :key="i" cols="12">
           <v-card outlined>
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
                 <v-card-title
                   class="subtitle-2"
-                  v-text="item.pertanyaan"
+                  v-text="soal.pertanyaan"
                 ></v-card-title>
-                <v-card-text class="caption" v-text="item.author"></v-card-text>
+                <v-card-text class="caption" v-text="soal.author"></v-card-text>
                 <v-card-actions>
-                  <v-btn small color="primary" @click="insert_existing(item)"
+                  <v-btn small color="primary" @click="insert_existing(soal)"
                     >Tambahkan</v-btn
                   >
                 </v-card-actions>
@@ -225,146 +203,69 @@ import axios from 'axios'
 export default {
   data: () => ({
     kuliah: [],
-    // matakuliah: [
-    //   {
-    //     nomor: 1,
-    //     nama: 'Konsep Pemrograman'
-    //   },
-    //   {
-    //     nomor: 2,
-    //     nama: 'Basis Data'
-    //   }
-    // ],
-
-    // datepicker
-    date: new Date().toISOString().substr(0, 10),
-    modal: false,
-    menuDate: false,
-
-    select: null,
-    items: ['A', 'B', 'C', 'D'],
-
-    checkbox: false,
-    radio_grup: 'radio1',
-    radio1: ' radio dari data',
-
-    soal: [],
-    editedItem: {
-      kuliah: '',
+    menuDatePicker: false,
+    kuncis: ['A', 'B', 'C', 'D'],
+    tipe_soal: '',
+    radio1: '',
+    form: {
+      soal: [],
+      kuliah: null,
       judul: '',
       date_created: '',
-      durasi: ''
+      durasi: '',
+      creator: null
     },
-
     // expansion panels
     lines: [],
-    blockRemoval: true
+    soal_tersedia: []
   }),
-  mounted() {
-    // this.addLine()
-    this.editedItem.creator = this.$auth.user.nomor
-    axios
-      .get(
-        'http://localhost:8000/matakuliah/cari_all?user=' +
-          this.$auth.user.nomor
-      )
-      .then((resp) => {
-        this.kuliah = resp.data
-        console.log(this.matakuliah)
-      })
-
-    if (this.editedItem.kuliah) {
-      axios
-        .get(
-          'http://localhost:8000/soal/cari_all?user=' +
-            this.$auth.user.nomor +
-            '&kuliah=' +
-            this.editedItem.kuliah
-        )
-        .then((resp) => {
-          this.items = resp.data
-        })
-    } else {
-      axios
-        .get(
-          'http://localhost:8000/soal/cari_all?user=' + this.$auth.user.nomor
-        )
-        .then((resp) => {
-          this.items = resp.data
-        })
-    }
-  },
-
-  methods: {
-    refresh_dropdown(e) {
-      if (this.editedItem.kuliah) {
+  watch: {
+    'form.kuliah'(v) {
+      if (v && process.browser) {
         axios
           .get(
             'http://localhost:8000/soal/cari_all?user=' +
               this.$auth.user.nomor +
               '&matakuliah=' +
-              this.editedItem.kuliah
+              v
           )
           .then((resp) => {
-            this.items = resp.data
-          })
-      } else {
-        axios
-          .get(
-            'http://localhost:8000/soal/cari_all?user=' + this.$auth.user.nomor
-          )
-          .then((resp) => {
-            this.items = resp.data
+            this.soal_tersedia = resp.data
           })
       }
-    },
+    }
+  },
+  mounted() {
+    // this.addLine()
+    this.form.creator = this.$auth.user.nomor
+    axios
+      .get('http://localhost:8000/matakuliah/cari_all?user=' + this.$auth.nomor)
+      .then((resp) => {
+        this.kuliah = resp.data
+      })
+  },
+
+  methods: {
     insert_existing(data) {
       this.lines.push(data)
     },
-
-    getValueRadioGrup(v) {
-      this.radio1 = v
-    },
-    validate() {
-      if (this.$refs.form.validate()) {
-        this.snackbar = true
-      }
-    },
-    reset() {
-      this.$refs.form.reset()
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation()
-    },
     addQuestion() {
-      // const checkEmptyLines = this.lines.filter((line) => line.number === null)
-      if (
-        // checkEmptyLines.length >= 0 &&
-        // this.lines.length > 0 &&
-        this.radio1 === 'Pilihan Ganda'
-      ) {
+      if (this.tipe_soal === 'Pilihan Ganda') {
         this.lines.push({
           tipe: 'Pilihan Ganda'
         })
-      } else if (
-        // checkEmptyLines.length >= 0 &&
-        // this.lines.length > 0 &&
-        this.radio1 === 'Essay'
-      ) {
+      } else if (this.tipe_soal === 'Essay') {
         this.lines.push({
           tipe: 'Essay'
         })
       }
     },
     removeQuestion(lineId) {
-      if (this.blockRemoval) this.lines.splice(lineId, 1)
+      if (this.form.soal.length > 1) this.lines.splice(lineId, 1)
     },
-
     save() {
-      console.log('---------------------------------aaaaaaa')
-      this.soal.push(this.editedItem)
-      // this.close()
-      const data = { ...this.editedItem }
+      // this.soal.push(this.form)
+      const data = { ...this.form }
       data.pertanyaans = this.lines
       data.pertanyaans.forEach((element, idx) => {
         data.pertanyaans[idx].matakuliah = data.kuliah
@@ -372,9 +273,9 @@ export default {
           data.pertanyaans[idx].creator = this.$auth.user.nomor
         }
       })
-      axios.post('http://localhost:8000/kuis', data).then((resp) => {
-        console.log(this.lines)
-      })((window.location = 'daftar'))
+      axios.post('http://localhost:8000/kuis', data).then((resp) => {})(
+        (window.location = 'daftar')
+      )
     }
   }
 }
--- pages/quiz/create/index.vue
+++ pages/quiz/create/index.vue
@@ -4,21 +4,15 @@
       <v-col>
         <create-quiz></create-quiz>
       </v-col>
-
-      <!-- <v-col cols="4">
-        <insert-bank-soal></insert-bank-soal>
-      </v-col>-->
     </v-row>
   </v-card>
 </template>
 
 <script>
 import CreateQuiz from '~/components/QuizDosen/MultipleGenerator.vue'
-// import InsertBankSoal from '~/components/QuizDosen/InsertBankSoal.vue'
 export default {
   components: {
     CreateQuiz
-    // InsertBankSoal
   }
 }
 </script>
